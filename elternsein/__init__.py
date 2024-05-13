7302c9e02c70771810152308b39bc70b90a9d4f1
Andreas Domberg <andom@posteo.de>
2024-05-13 22:20:53 +0200

    add import section

--- elternsein/__init__.py
+++ elternsein/__init__.py
@@ -3,10 +3,19 @@ from pathlib import Path
 import logging
 
 from flask import Flask
+import numpy as np
 import pandas as pd
+import geopandas as gpd
 from dash import Dash, dcc, html, Input, Output, State, callback, dash_table
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+
+# from data.sources import destatis_sources, bkg_source
+from .colors import color_rgba
+from .utils import cuyo, num
 
 # import from config relatively, so it remains portable:
 dashapp_rootdir = Path(__file__).resolve().parents[1]
