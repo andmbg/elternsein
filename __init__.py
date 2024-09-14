a8670d2e8d345c5c91f3c4aec1beb7076df7665e
Andreas Domberg <andom@posteo.de>
2024-09-14 09:48:14 +0200

    init i18n branch

--- __init__.py
+++ __init__.py
@@ -1,6 +1,15 @@
 metadata = {
-    "title": "Elterngeld: Paid Parental Leave in Germany",
-    "route": "/en/elternsein/",
-    "thumbnail": "dashapps/en/elternsein/preview.png",
-    "synopsis": "The purpose of paid parental leave is to stop treating parenting and professional success as mutually exclusive. How is this option being accepted -- by mothers and especially, fathers? Can it contribute to modernizing gender roles when it comes to child care?",
+    "de": {
+        "title": "Elterngeld: Paid Parental Leave in Germany",
+        "route": "/de/elternsein/",
+        "thumbnail": "dashapps/en/elternsein/preview.png",
+        "synopsis": "Aufgabe des Elterngelds ist es, Elternschaft und beruflichen Erfolg nicht länger als gegensätzliche Optionen zu haben. Wie wird es angenommen - von Müttern und besonders Vätern? Kann es dazu beitragen, Geschlechterrollen in der Kindeserziehung zu modernisieren?",
+    },
+    "en": {
+        "title": "Elterngeld: Paid Parental Leave in Germany",
+        "route": "/en/elternsein/",
+        "thumbnail": "dashapps/en/elternsein/preview.png",
+        "synopsis": "The purpose of paid parental leave is to stop treating parenting and professional success as mutually exclusive. How is this option being accepted -- by mothers and especially, fathers? Can it contribute to modernizing gender roles when it comes to child care?",
+    },
+    
 }
